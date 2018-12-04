67bf2235de2d06c535bc289848fca956367664e2
Raoni Demarchi <raonidemarchi@gmail.com>
2018-12-04 15:23:59 -0200

    first push

---
+++ App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { View, Text, Button, ScrollView } from 'react-native'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.setState({
+      phrase: null
+    })
+
+    this.geraNumeroAleatorio().bind(this)
+  }
+
+  componentWillMount() {
+    // alert('tyeste')
+  }
+
+  geraNumeroAleatorio = () => {
+    const randomIndex = Math.floor(Math.random() * 5)
+    const phrases = [
+      'Frase 1',
+      'Frase 2',
+      'Frase 3',
+      'Frase 4',
+      'Frase 5'
+    ]
+
+    this.phrases.setState(phrases[randomIndex])
+  }
+
+  render() {
+    const { phrase } = this.state
+
+    return (
+      <View style={styles.view}>
+        <ScrollView style={styles.scrollView}>
+          <Text style={styles.title}>Meu primeiro app</Text>
+          <Button title="Gerar número aleatório" onPress={this.geraNumeroAleatorio} style={styles.button} />
+        </ScrollView>
+        
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Rodapé</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = {
+  view: {
+    flex: 1,
+    backgroundColor: '#ffee58'
+  },
+  scrollView: {
+    padding: 16
+  },
+  footer: {
+    padding: 16,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-end'
+  },
+  footerText: {
+    fontSize: 20
+  },
+  title: {
+    marginBottom: 14,
+    fontSize: 28
+  },
+  button: {
+    color: '#fff'
+  }
+}
